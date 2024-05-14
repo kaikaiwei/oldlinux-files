69243f8f2a20a8e9d9b1dc6ae7c46ae139b7cdcd
weika <137067401@qq.com>
2024-05-15 07:10:05 +0800

    ams.s

--- Linux-0.11/sources/system/linux-0.11/kernel/asm.s
+++ Linux-0.11/sources/system/linux-0.11/kernel/asm.s
@@ -11,34 +11,42 @@
  * the fpu must be properly saved/resored. This hasn't been tested.
  */
 
+/**
+ * 包括大部分cpu探测到的异常故障处理的底层代码，包括数字协处理器（FPU）的异常处理。
+ * 主要处理方式是在中断处理程序中，卓当调用相应的c函数程序，显示出错位置和出错号，然后退出中断
+ * 本代码主要设计0-16号中断处理，17-31保留。
+ */
+
+# 全局函数声明，原型在traps.c中。
 .globl _divide_error,_debug,_nmi,_int3,_overflow,_bounds,_invalid_op
 .globl _double_fault,_coprocessor_segment_overrun
 .globl _invalid_TSS,_segment_not_present,_stack_segment
 .globl _general_protection,_coprocessor_error,_irq13,_reserved
 
+# int0， 除0异常。
 _divide_error:
-	pushl $_do_divide_error
-no_error_code:
-	xchgl %eax,(%esp)
-	pushl %ebx
-	pushl %ecx
-	pushl %edx
-	pushl %edi
-	pushl %esi
-	pushl %ebp
-	push %ds
-	push %es
-	push %fs
-	pushl $0		# "error code"
-	lea 44(%esp),%edx
-	pushl %edx
-	movl $0x10,%edx
-	mov %dx,%ds
-	mov %dx,%es
-	mov %dx,%fs
-	call *%eax
-	addl $8,%esp
-	pop %fs
+	pushl $_do_divide_error	# 首先把处理函数入栈
+no_error_code:				# 这里是无出错号的处理。
+	xchgl %eax,(%esp)		# 将esp保存到eax中，eax被交换入栈。
+	pushl %ebx				# ebx入栈
+	pushl %ecx				# ecx入栈
+	pushl %edx				# edx入栈
+	pushl %edi				# edi入栈
+	pushl %esi				# esi入栈
+	pushl %ebp				# ebp入栈
+	push %ds				# ds入栈
+	push %es				# es入栈
+	push %fs				# fs入栈
+	pushl $0		# "error code"， 出错号入栈	
+	lea 44(%esp),%edx		# 取原调用返回地址处堆栈指针位置，并压入堆栈。esp+44字节的地址复制给edx。
+	pushl %edx				# edx（原调用函数返回地址）入栈
+	movl $0x10,%edx			# edx为0x10， 表示内核数据段选择符。
+	mov %dx,%ds				# ds为内核数据段选择符
+	mov %dx,%es				# es为内核数据段选择符
+	mov %dx,%fs				# fs为内核数据段选择符
+	call *%eax				# 调用eax，也就是_do_divide_error
+	addl $8,%esp			# c函数返回后，esp+8，刚好移除掉连个参数的位置。即addr（原调用函数的返回地址）和error_code
+	pop %fs					# 出栈操作
 	pop %es
 	pop %ds
 	popl %ebp
@@ -82,18 +90,21 @@ _reserved:
 	pushl $_do_reserved
 	jmp no_error_code
 
-_irq13:
-	pushl %eax
-	xorb %al,%al
-	outb %al,$0xF0
-	movb $0x20,%al
-	outb %al,$0x20
-	jmp 1f
+# 下面用于处理协处理器执行完一个操作是就会发出irq13中断信号，以通知cpu操作2岸城
+_irq13:					# int45（0x20+13），数字协处理器发出的中断
+	pushl %eax			
+	xorb %al,%al		# 80387在执行计算时，cpu会等待其操作的完成。al为0
+	outb %al,$0xF0		# 将al
+	movb $0x20,%al		# al=0x20
+	outb %al,$0x20		# 向主中断控制器发送eoi（中断结束）信号
+	jmp 1f				# 这两个指令起到延时作用
 1:	jmp 1f
-1:	outb %al,$0xA0
-	popl %eax
-	jmp _coprocessor_error
+1:	outb %al,$0xA0		# 将8025从中断控制芯片发送eoi（中断结束）信号
+	popl %eax			# eax出栈
+	jmp _coprocessor_error	# 调用kernel/system_call.s中_coprocessor_error函数
 
+# 以下中断在调用时会在中断返回地址后面将出错号压入堆栈，因此返回时也需要将错误号弹出
+# int8 双输错故障。
 _double_fault:
 	pushl $_do_double_fault
 error_code:
@@ -108,14 +119,14 @@ error_code:
 	push %es
 	push %fs
 	pushl %eax			# error code
-	lea 44(%esp),%eax		# offset
+	lea 44(%esp),%eax		# offset， 程序返回地址处堆栈指针位置入栈
 	pushl %eax
-	movl $0x10,%eax
+	movl $0x10,%eax			# 置内核数据段选择符
 	mov %ax,%ds
 	mov %ax,%es
 	mov %ax,%fs
-	call *%ebx
-	addl $8,%esp
+	call *%ebx				# 调用相应的c函数，其参数已尽刚入栈。
+	addl $8,%esp			# 堆栈指针重新指向栈中方fs内容的位置
 	pop %fs
 	pop %es
 	pop %ds
@@ -128,19 +139,19 @@ error_code:
 	popl %eax
 	iret
 
-_invalid_TSS:
+_invalid_TSS:			# int10 无效的任务状态段
 	pushl $_do_invalid_TSS
 	jmp error_code
 
-_segment_not_present:
+_segment_not_present:	# int11 段不存在
 	pushl $_do_segment_not_present
 	jmp error_code
 
-_stack_segment:
+_stack_segment:			# int12 堆栈段错误
 	pushl $_do_stack_segment
 	jmp error_code
 
-_general_protection:
+_general_protection:	# int13 一般保护性错误
 	pushl $_do_general_protection
 	jmp error_code
 
--- Linux-0.11/sources/system/linux-0.11/kernel/traps.c
+++ Linux-0.11/sources/system/linux-0.11/kernel/traps.c
@@ -9,6 +9,8 @@
  * state in 'asm.s'. Currently mostly a debugging-aid, will be extended
  * to mainly kill the offending process (probably by giving it a signal,
  * but possibly by killing it outright if necessary).
+ * 主要包括一些在处理异常故障（硬件中断）的底层代码asm.s中调用的响应C函数。
+ * 用于显示出错信息和出错号等调试信息。 die函数用于在中断处理中显示详细的出错信息。trap_init函数用于硬件异常处理中断向量的初始化，并设置允许中断信号的到来。
  */
 #include <string.h>
 
@@ -19,65 +21,97 @@
 #include <asm/segment.h>
 #include <asm/io.h>
 
+/** 
+ * 嵌入式汇编语句。取段seg中地址addr处的一个字节。
+ * 在栈中保存fs，将ax移动到fs中，将fs：addr转移到al中，fs出栈
+ * 圆括号作为表达式使用
+ */
 #define get_seg_byte(seg,addr) ({ \
 register char __res; \
 __asm__("push %%fs;mov %%ax,%%fs;movb %%fs:%2,%%al;pop %%fs" \
 	:"=a" (__res):"0" (seg),"m" (*(addr))); \
 __res;})
 
+/** 
+ * 嵌入式汇编语句。取段seg中地址addr处的一个长字（4字节）
+ * 在栈中保存fs，将ax移动到fs中，将fs：addr转移到al中，fs出栈
+ * 圆括号作为表达式使用
+ */
 #define get_seg_long(seg,addr) ({ \
 register unsigned long __res; \
 __asm__("push %%fs;mov %%ax,%%fs;movl %%fs:%2,%%eax;pop %%fs" \
 	:"=a" (__res):"0" (seg),"m" (*(addr))); \
 __res;})
 
+/** 
+ * 嵌入式汇编语句。取fs寄存器的值。
+ * 将fs保存在ax中，
+ * 圆括号作为表达式使用
+ */
 #define _fs() ({ \
 register unsigned short __res; \
 __asm__("mov %%fs,%%ax":"=a" (__res):); \
 __res;})
 
-int do_exit(long code);
+// 下面是一些函数原型。
+int do_exit(long code);		// 程序推出操作。在kernel/exit.c
 
-void page_exception(void);
 
-void divide_error(void);
-void debug(void);
-void nmi(void);
-void int3(void);
-void overflow(void);
-void bounds(void);
-void invalid_op(void);
-void device_not_available(void);
-void double_fault(void);
-void coprocessor_segment_overrun(void);
-void invalid_TSS(void);
-void segment_not_present(void);
-void stack_segment(void);
-void general_protection(void);
-void page_fault(void);
-void coprocessor_error(void);
-void reserved(void);
-void parallel_interrupt(void);
+void page_exception(void);	// 页异常，世纪时page_fault， 命名，pages.s中
+
+// 中断处理程序的原型，代码在kernel/asm.s或systemcall.s中。
+void divide_error(void);	// int0， kernel/ams.s
+void debug(void);			// int1， kernel/ams.s
+void nmi(void);				// int2， kernel/ams.s
+void int3(void);			// int3， kernel/ams.s
+void overflow(void);		// int4， kernel/ams.s
+void bounds(void);			// int5， kernel/ams.s
+void invalid_op(void);		// int6， kernel/ams.s
+void device_not_available(void);	// int7， kernel/ams.s
+void double_fault(void);			// int8， kernel/ams.s
+void coprocessor_segment_overrun(void);	// int9， kernel/ams.s
+void invalid_TSS(void);				// int10， kernel/ams.s
+void segment_not_present(void);		// int11， kernel/ams.s
+void stack_segment(void);			// int12， kernel/ams.s
+void general_protection(void);		// int13， kernel/ams.s
+void page_fault(void);				// int14， mm/pages.s
+void coprocessor_error(void);		// int16， kernel/ams.s
+void reserved(void);				// int39， kernel/system_call.s
+void parallel_interrupt(void);		// int45，协处理器中断处理 kernel/ams.s
 void irq13(void);
 
+/**
+ * 该子程序用来打印出错中断的名称，出错号，调用程序的eip,eflags,esp,fs段寄存器值，
+ * 段的基址，段的长度，进程号pid，任务号，10字节指令吗，如果堆栈在用户端，还打印16字节的堆栈内容
+ * @param str
+ * @param esp
+ * @param nr
+ */
 static void die(char * str,long esp_ptr,long nr)
 {
 	long * esp = (long *) esp_ptr;
 	int i;
 
-	printk("%s: %04x\n\r",str,nr&0xffff);
+	printk("%s: %04x\n\r",str,nr&0xffff); // 打印出错信息和nr（任务号）
+	/// 打印eip， eflags，esp
 	printk("EIP:\t%04x:%p\nEFLAGS:\t%p\nESP:\t%04x:%p\n",
 		esp[1],esp[0],esp[2],esp[4],esp[3]);
+	// 打印fs段寄存器内容
 	printk("fs: %04x\n",_fs());
+	// 打印base指针，ldtr。 和limit
 	printk("base: %p, limit: %p\n",get_base(current->ldt[1]),get_limit(0x17));
+	// 如果是用户堆栈。
 	if (esp[4] == 0x17) {
+		// 打印16字节的堆栈信息
 		printk("Stack: ");
 		for (i=0;i<4;i++)
 			printk("%p ",get_seg_long(0x17,i+(long *)esp[3]));
 		printk("\n");
 	}
-	str(i);
+	str(i);	// 去当前运行任务的任务号。include/linux/sched.h
+	// 打印pid和任务号
 	printk("Pid: %d, process nr: %d\n\r",current->pid,0xffff & i);
+	// 打印10字节的指令码
 	for(i=0;i<10;i++)
 		printk("%02x ",0xff & get_seg_byte(esp[1],(i+(char *)esp[0])));
 	printk("\n\r");
@@ -106,7 +140,7 @@ void do_int3(long * esp, long error_code,
 {
 	int tr;
 
-	__asm__("str %%ax":"=a" (tr):"0" (0));
+	__asm__("str %%ax":"=a" (tr):"0" (0)); // 取任务寄存器
 	printk("eax\t\tebx\t\tecx\t\tedx\n\r%8x\t%8x\t%8x\t%8x\n\r",
 		eax,ebx,ecx,edx);
 	printk("esi\t\tedi\t\tebp\t\tesp\n\r%8x\t%8x\t%8x\t%8x\n\r",
@@ -178,11 +212,17 @@ void do_reserved(long esp, long error_code)
 	die("reserved (15,17-47) error",esp,error_code);
 }
 
+/**
+ * 陷阱门（中断向量）初始化
+ * set_trap_gate和set_system_gate的区别在于前者设置的特权级位3，后者为0
+ * 因此断点int3，溢出overflow，边界出错中断bounds可有任何程序产生。
+ * 这两个函数是嵌入式汇编宏，在include/asm/system.h中。
+ */
 void trap_init(void)
 {
 	int i;
 
-	set_trap_gate(0,&divide_error);
+	set_trap_gate(0,&divide_error);		// 设置除中断的向量
 	set_trap_gate(1,&debug);
 	set_trap_gate(2,&nmi);
 	set_system_gate(3,&int3);	/* int3-5 can be called from all */
@@ -199,10 +239,11 @@ void trap_init(void)
 	set_trap_gate(14,&page_fault);
 	set_trap_gate(15,&reserved);
 	set_trap_gate(16,&coprocessor_error);
+	// 将17到48的陷阱门设置为reserved。在之后的硬件初始化时会重新设置自己的陷阱门。
 	for (i=17;i<48;i++)
 		set_trap_gate(i,&reserved);
-	set_trap_gate(45,&irq13);
-	outb_p(inb_p(0x21)&0xfb,0x21);
-	outb(inb_p(0xA1)&0xdf,0xA1);
-	set_trap_gate(39,&parallel_interrupt);
+	set_trap_gate(45,&irq13);		// 设置数字协处理器
+	outb_p(inb_p(0x21)&0xfb,0x21);	// 允许主8259A芯片irq2中断
+	outb(inb_p(0xA1)&0xdf,0xA1);	// 允许主8259A芯片irq3中断
+	set_trap_gate(39,&parallel_interrupt);	// 设置并行口陷阱门。
 }
