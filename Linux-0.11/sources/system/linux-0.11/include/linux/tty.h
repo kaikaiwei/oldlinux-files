d93a803bc8ee760734e3067b2a909855028b0af8
weika <137067401@qq.com>
2024-06-10 13:46:04 +0800

    tty

--- Linux-0.11/sources/system/linux-0.11/include/linux/tty.h
+++ Linux-0.11/sources/system/linux-0.11/include/linux/tty.h
@@ -11,14 +11,18 @@
 
 #include <termios.h>
 
+// 队列缓冲区长度
 #define TTY_BUF_SIZE 1024
 
+/**
+ * 字符缓冲队列中。也叫字符标。
+ */
 struct tty_queue {
-	unsigned long data;
-	unsigned long head;
-	unsigned long tail;
-	struct task_struct * proc_list;
-	char buf[TTY_BUF_SIZE];
+	unsigned long data;		// 等待队列缓冲区中当前数据统计值。对于串口终端，存放串口端口地址
+	unsigned long head;		// 缓冲区中数据头指针
+	unsigned long tail;		// 缓冲区中数据尾指针
+	struct task_struct * proc_list;	// 等待本缓冲队列的进程列表
+	char buf[TTY_BUF_SIZE];		// 队列的缓冲区。 长度1KB。
 };
 
 #define INC(a) ((a) = ((a)+1) & (TTY_BUF_SIZE-1))
@@ -42,16 +46,20 @@ struct tty_queue {
 #define STOP_CHAR(tty) ((tty)->termios.c_cc[VSTOP])
 #define SUSPEND_CHAR(tty) ((tty)->termios.c_cc[VSUSP])
 
+/**
+ * 终端设备结构。
+ *
+ */
 struct tty_struct {
-	struct termios termios;
-	int pgrp;
-	int stopped;
-	void (*write)(struct tty_struct * tty);
-	struct tty_queue read_q;
-	struct tty_queue write_q;
-	struct tty_queue secondary;
+	struct termios termios;			// 终端io属性和控制字符数组结构
+	int pgrp;						// 所属进程组
+	int stopped;					// 停止标志
+	void (*write)(struct tty_struct * tty);	// tty 写函数指针
+	struct tty_queue read_q;		// tty 读队列。存放从键盘或者串行终端输入的原始字符序列
+	struct tty_queue write_q;		// tty 写队列。存放写到控制台显示屏或串行终端程序处理（过滤过的）数据
+	struct tty_queue secondary;		// tty辅助队列。存放规范模式字符序列。规范模式队列。 存放read_q中取出经过行规则程序处理过的数据。上层tty_read用于读取secondary队列中的字符。
 	};
-
+// tty数据结构数组
 extern struct tty_struct tty_table[];
 
 /*	intr=^C		quit=^|		erase=del	kill=^U
